ea60d7f9550cb55348eb67c49fca2019bfdc0ed4
harikrishna19 <krishna19599@gmail.com>
2025-03-30 23:26:23 +0530

    Updated geom_tile graph Salah Plot summary

--- R/final_infographic.R
+++ R/final_infographic.R
@@ -6,7 +6,8 @@ gr<-calculate_goal_ranges(data=liv_match_summaries,team_name = "Liverpool",team_
 gs<-calculate_goals_assists(data = liv_match_summaries,team_name="Liverpool")
 table_df
 
-build_pitch
+build_pitch+gr+gs+test_plot+wrap_elements(full = table_df)
 
 
-(build_pitch+gr/gs) / wrap_elements(full = table_grob)
+(table_df+gr/gs) / wrap_elements(full = build_pitch)+test_plot
+build_pitch+gr+gs+final_plot
