30673cdf0158cdadbfb3177e1572c09e99bd145c
harikrishna19 <krishna19599@gmail.com>
2025-08-18 23:33:27 +0530

    Initial commit-Test

---
+++ R/03-PlayingXI-Analysis-Spurs/data_pull.R
@@ -0,0 +1,10 @@
+
+# Loading required packages for analysis ----------------------------------
+
+library(worldfootballR)
+library(tidyverse)
+
+
+# Load the required data for analysis -------------------------------------
+
+
