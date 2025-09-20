37bdd19056d187db66cdc546a634356195bb4973
harikrishna19 <krishna19599@gmail.com>
2025-09-20 22:46:57 +0530

    Updated utils & data-explore.R

--- R/04-Transfer-Markt-2025-26-season/utils.R
+++ R/04-Transfer-Markt-2025-26-season/utils.R
@@ -16,6 +16,29 @@ teams<-c("Arsenal","Aston Villa","AFC Bournemouth","Brentford","Brighton","Burnl
          "Manchester City","Manchester United","NewCastle United","Nottingham Forest",
          "Sunderland AFC","Tottenham HotSpur","West Ham","Wolves")
 
+team_colors <- c(
+  "Arsenal"           = "#f08080",  # lighter red
+  "Aston Villa"       = "#b2a1c7",  
+  "AFC Bournemouth"   = "#f5a1a1",  
+  "Brentford"         = "#f7a8a8",  
+  "Brighton"          = "#7abaff",  
+  "Burnley"           = "#d4a6a6",  
+  "Chelsea"           = "#6aa5ff",  
+  "Crystal Palace"    = "#9dbdf2",  
+  "Everton"           = "#77aadd",  
+  "Fulham"            = "#d9d9d9",  
+  "Leeds United"      = "#f6f6b2",  
+  "Liverpool"         = "#ff8c8c",  
+  "Manchester City"   = "#b0e0ff",  
+  "Manchester United" = "#ff9d9d",  
+  "Newcastle United"  = "#d0d0d0",  
+  "Nottingham Forest" = "#ff9b9b",  
+  "Sunderland AFC"    = "#f59c9c",  
+  "Tottenham Hotspur" = "#c2c2ff",  
+  "West Ham"          = "#d2a6c7",  
+  "Wolves"            = "#ffd27f"   
+)
+
 
 # Clean  data ---------------------------------------
 clean_dataset <- function(data, keep_order = NULL, drop = NULL,player_name_col=NULL) {
