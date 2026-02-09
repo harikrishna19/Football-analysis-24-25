9ebb77b0715cd645cf1665454bc93555cafe35b1
harikrishna19 <krishna19599@gmail.com>
2026-02-09 13:48:30 +0530

    Update labels & text for annotate

--- R/04-Transfer-Markt-2025-26-season/draft_plot.R
+++ R/04-Transfer-Markt-2025-26-season/draft_plot.R
@@ -1,27 +1,40 @@
 
-scraped_data<-
-  scrape %>%   
-  arrange(exp) %>% 
+
+scraped_data <-
+  scrape %>%
+  arrange(exp) %>%
   mutate(
-  id   = row_number(),
-  frac = exp / 200 ,    # scale to 0–1
-  frac_plot = ifelse(frac == 0, 0.005, frac),
-  zero_flag = frac == 0,
-  label_check=paste0(club,"-",exp,"M")
-)
+    id   = row_number(),
+    frac = exp / 200 ,
+    # scale to 0–1
+    frac_plot = ifelse(frac == 0, 0.005, frac),
+    zero_flag = frac == 0,
+    label_check = ifelse(exp!=0,paste0(club, "-", exp, "M"),club)
+  )
 
 club_cols <- c(
-  "Brighton & Hove Albion"  = "#0057B8",  # Blue
-  "Nottingham Forest"       = "#DD0000",  # Red
-  "Wolverhampton Wanderers" = "#FDB913",  # Old Gold
-  "Brentford FC"            = "#E30613",  # Red
-  "Sunderland AFC"          = "#EB172B",  # Red
-  "Fulham FC"               = "#000000",  # Black
-  "Aston Villa"             = "#95BFE5",  # Claret & Blue (blue tone)
-  "AFC Bournemouth"         = "#DA291C",  # Red
-  "Tottenham Hotspur"       = "#132257",  # Navy
-  "West Ham United"         = "#7A263A",  # Claret
-  "Crystal Palace"          = "#1B458F",  # Blue
+  "Brighton & Hove Albion"  = "#0057B8",
+  # Blue
+  "Nottingham Forest"       = "#DD0000",
+  # Red
+  "Wolverhampton Wanderers" = "#FDB913",
+  # Old Gold
+  "Brentford FC"            = "#E30613",
+  # Red
+  "Sunderland AFC"          = "#EB172B",
+  # Red
+  "Fulham FC"               = "#000000",
+  # Black
+  "Aston Villa"             = "#95BFE5",
+  # Claret & Blue (blue tone)
+  "AFC Bournemouth"         = "#DA291C",
+  # Red
+  "Tottenham Hotspur"       = "#132257",
+  # Navy
+  "West Ham United"         = "#7A263A",
+  # Claret
+  "Crystal Palace"          = "#1B458F",
+  # Blue
   "Manchester City"         = "#6CABDD"   # Sky Blue
 )
 
@@ -29,7 +42,8 @@ club_cols <- c(
 ggplot(scraped_data) +
   geom_arc_bar(
     aes(
-      x0 = 0, y0 = 0,
+      x0 = 0,
+      y0 = 0,
       r0 = id - 0.35,
       r  = id + 0.25,
       start = pi * 0.01,
@@ -47,41 +61,34 @@ ggplot(scraped_data) +
     ymax = 0.5,
     fill = "red",
     colour = "black",
-    linewidth = 0.5
+    linewidth = 0.5,
+    alpha=0.08
   ) +
-  geom_text(aes(x=-4.0,y=id,label=label_check),size = 2,color="black")+
+  geom_text(aes(x = -4.0, y = id, label = label_check),
+            size = 2,
+            color = "black",fontface="bold") +
   annotate(
     "text",
-    x = 5.5, y = -0.5,
+    x = 5.5,
+    y = -0.5,
     label = "Premier League:~453 Million",
     size = 5,
     color = "grey30"
   ) +
+  annotate(
+    "text",
+    x = -12.5,
+    y = 3.5,
+    label = "Teams with no spends",
+    size = 3,
+    color = "red",fontface="bold"
+  ) +
   coord_fixed(clip = "off") +
-  # scale_alpha_manual(values = c(`TRUE` = 0.4, `FALSE` = 1))+
   scale_fill_manual(values = club_cols) +
-  theme_void()+
-  theme(legend.position = "none")+
-  labs(title = "Premier League Winter Spending of 2025/26",
-       subtitle = "Manchester City have spent most of the money in 2025/26 season",caption = "Viz by Hari Krishna",tag = "New")
-
-# sample bar plot
-
-
-# Load the ggplot2 library
-library(ggplot2)
-
-# # Create a sample data frame
-# data <- data.frame(
-#   name = c("Group A", "Group B", "Group C", "Group D", "Group E"),
-#   value = c(3, 12, 5, 18, 45)
-# )
-# 
-# # Generate the bar plot
-# ggplot(data, aes(x = name, y = value)) +
-#   geom_bar(stat = "identity", fill = "skyblue") +
-#   labs(title = "Sample Bar Plot of Values by Group",
-#        x = "Group",
-#        y = "Value") +
-#   geom_text(aes(x=0,y=1,label="TEst"))+
-#   theme_minimal()+coord_flip()
+  theme_void() +
+  theme(legend.position = "none") +
+  labs(
+    title = "Premier League Winter Spending of 2025/26",
+    subtitle = "Manchester City have spent most of the money in 2025/26 season",
+    caption = "Viz by Hari Krishna"
+  )
