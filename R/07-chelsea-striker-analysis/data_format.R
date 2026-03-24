b23a22de1fc19a51c2ba96372eb05b5f36e10a56
harikrishna19 <krishna19599@gmail.com>
2026-03-24 22:15:15 +0530

    calculated total goals by season

--- R/07-chelsea-striker-analysis/data_format.R
+++ R/07-chelsea-striker-analysis/data_format.R
@@ -11,7 +11,7 @@ chelsea_data<-read.csv("data/chelsea_data.csv")
 
 cols_to_parse <- c("h", "a", "goals", "xG","forecast")
 
-team_data <- team_data %>% filter(as.Date(datetime)<Sys.Date()) %>%
+team_data <- team_data %>% filter(as.Date(datetime)<Sys.Date() & result!= "") %>%
   mutate(across(all_of(cols_to_parse),
                 ~ gsub("'", '"', .))) %>%
   mutate(across(all_of(cols_to_parse),
@@ -23,5 +23,11 @@ team_data <- team_data %>% filter(as.Date(datetime)<Sys.Date()) %>%
   }
 
 
-team_data |> dplyr::filter(h_short_title=="CHE"|season==2016) |> nrow()
+total_goals <- team_data %>%
+  filter(h_title == "Chelsea" | a_title == "Chelsea") %>%
+  mutate(goals_scored = ifelse(h_title == "Chelsea", goals_h, goals_a)) %>%
+  group_by(season) %>% 
+  summarise(total_goals = sum(as.numeric(goals_scored), na.rm = TRUE))
+
+
 
